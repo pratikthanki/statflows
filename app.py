4ee445fdd37fece1605d5eefecd9f44b43e8248d
Pratik <thankip9@outlook.com>
2018-11-11 20:54:09 +0000

    hover effect for players

--- app.py
+++ app.py
@@ -15,17 +15,53 @@ import dash_table_experiments as dt
 from Settings import *
 from Queries import *
 
-pd.set_option('display.max_columns', 20)
-pd.set_option('display.width', 500)
-
 
 app = dash.Dash()
 server = app.server
 app.scripts.config.serve_locally = True
 dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-finance-1.28.0.min.js'
 
+app.head = [
+    ('''
+
+    <style type="text/css">
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 100%;
+        width: 100%;
+        opacity: 0;
+        transition: .5s ease;
+        background-color: #0f6db5;
+    }
+
+    .container:hover .overlay {
+        opacity: .9;
+    }
+
+    .text {
+        color: white;
+        font-size: 20px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
+     </style>
+
+    ''')
+]
 
 # Establish database connection to Write Records
+
+
 def SQLServerConnection(config):
     conn_str = (
         'DRIVER={driver};SERVER={server},{port};DATABASE={database};UID={username};PWD={password}')
@@ -76,28 +112,32 @@ COLORS = [
     {'background': '#d9d9d9', 'text': 'gainsboro'}]
 
 
-headerstyle = {'align': 'center',
-               'width': '300px',
-               'background-color': '#0f6db5',
-               'text-align': 'center',
-               'font-size': '22px',
-               'padding': '5px',
-               'color': '#ffffff'}
+headerstyle = {
+    'align': 'center',
+    'width': '300px',
+    'background-color': '#0f6db5',
+    'text-align': 'center',
+    'font-size': '22px',
+    'padding': '5px',
+    'color': '#ffffff'}
 
-tablestyle = {'display': 'table',
-              'border-cllapse': 'separate',
-              'font': '15px Open Sans, Arial, sans-serif',
-              'font-weight': '30',
-              'border-collapse': 'separate'}
+tablestyle = {
+    'display': 'table',
+    'border-cllapse': 'separate',
+    'font': '15px Open Sans, Arial, sans-serif',
+    'font-weight': '30',
+    'border-collapse': 'separate'}
 
-rowstyle = {'color': 'black',
-            'align': 'center',
-            'text-align': 'center',
-            'font-size': '40px'}
+rowstyle = {
+    'color': 'black',
+    'align': 'center',
+    'text-align': 'center',
+    'font-size': '40px'}
 
 tabs_styles = {
     'height': '50px'
 }
+
 tab_style = {
     'borderBottom': '1px solid #d6d6d6',
     'padding': '5px',
@@ -114,10 +154,60 @@ tab_selected_style = {
     'font-size': '22px'
 }
 
+ulstyle = {
+    'list-style-type': 'none',
+    'columnCount': '2',
+    'column-width': '40px'
+}
+
+styleP = {
+    'font-size': '10px',
+    'text-align': 'center'
+}
+
+overlay_style = {
+    # 'position': 'absolute',
+    # 'top': '0',
+    # 'bottom': '0',
+    # 'left': '0',
+    # 'right': '0',
+    # 'height': '100%',
+    # 'width': '100%',
+    # 'opacity': '0',
+    # 'transition': '.5s ease',
+    # 'background-color': '#0f6db5'
+}
+
+
 def localImg(image):
     encoded_image = base64.b64encode(
         open(os.getcwd() + '/TeamLogos/' + image, 'rb').read())
     return 'data:image/png;base64,{}'.format(encoded_image)
+
+
+latest_df = loadData(latestGame)
+latest_df.columns = ['LatestDate', 'Venue', 'TeamID', 'PlayerID', 'FullName', 'JerseyNum', 'Pos', 'Min',
+                     'Pts', 'Ast', 'Blk', 'Reb', 'Fga', 'Fgm', 'Fta', 'Ftm', 'Stl', 'Tov', 'Pf', 'Pip', 'Pipa', 'Pipm']
+
+latest_df['FG%'] = (latest_df['Fgm'] / latest_df['Fga']) * 100
+latest_df['FT%'] = (latest_df['Ftm'] / latest_df['Fta']) * 100
+latest_df['Pip%'] = (latest_df['Pipm'] / latest_df['Pipa']) * 100
+
+
+def playerInfo(player):
+    row = []
+    cols = ['LatestDate', 'Min', 'Pts', 'Ast',
+            'Blk', 'Reb', 'Stl', 'FG%', 'FT%', 'Pip%']
+
+    df = latest_df.loc[latest_df['PlayerID'] == int(player)]
+
+    for col in df.columns:
+        if any(col in c for c in cols) and col != 'Pip':
+            text = col + ': ' + str(df.iloc[0][col])
+            P = html.P(text, style=styleP)
+            row.append(html.Li(P))
+
+    return html.Div(html.Ul(row, style=ulstyle), className='text')
 
 
 def getPlayerImage(player):
@@ -126,11 +216,13 @@ def getPlayerImage(player):
         name = teams.loc[teams['PlayerId'] == player, 'FullName'].iloc[0]
 
         return html.Div(children=[
-            html.Img(src=str(img), style={'height': '100px'}),
+            html.Img(src=str(img), style={
+                     'height': '100px'}, className='image'),
             html.Div(html.H4(str(name),
                              style={'font-size': '20px',
-                                    'text-align': 'center'}))
-        ])
+                                    'text-align': 'center'})),
+            html.Div(playerInfo(player), className='overlay', style=overlay_style)],
+            className='container', style={'width': '100%', 'position': 'relative'})
 
 
 def getTeamImage(team):
@@ -167,16 +259,11 @@ def get_data_object(df):
 # Pacific = ['GSW', 'LAC', 'LAL', 'PHX', 'SAC']
 # Southwest = ['DAL', 'HO', 'MEM', 'NOP', 'SAS']
 
-teamsQuery = 'SELECT * FROM TeamRosters2018'
-teams = loadData(teamsQuery)
-teams.columns = ['PlayerId', 'TeamId', 'TeamCode', 'FirstName', 'LastName', 'FullName', 'TeamLogo', 'PlayerImg', 'Division', 'Conference']
+teams = loadData(teamRosters)
+teams.columns = ['PlayerId', 'TeamId', 'TeamCode', 'FirstName', 'LastName',
+                 'FullName', 'TeamLogo', 'PlayerImg', 'Division', 'Conference']
 teamdf = parseTeams(teams)
 get_data_object(teamdf)
-
-
-latestGame = loadData(latestGameQuery)
-latestGame.columns = ['LatestDate', 'Venue', 'TeamID', 'PlayerID', 'FullName', 'JerseyNum', 'Pos', 'Min', 'Pts', 'Ast', 'Blk', 'Reb', 'Fga', 'Fgm', 'Fta', 'Ftm', 'Stl', 'Tov', 'Pf', 'Pip', 'Pipa', 'Pipm']
-
 
 
 def update_layout():
@@ -197,13 +284,19 @@ def update_layout():
                                            'padding': '10px'}),
 
         dcc.Tabs(id="div-tabs", value='Atlantic', children=[
-            dcc.Tab(label='ATLANTIC', value='Atlantic', style=tab_style, selected_style=tab_selected_style),
-            dcc.Tab(label='CENTRAL', value='Central', style=tab_style, selected_style=tab_selected_style),
-            dcc.Tab(label='SOUTHEAST', value='Southeast', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='ATLANTIC', value='Atlantic',
+                    style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='CENTRAL', value='Central', style=tab_style,
+                    selected_style=tab_selected_style),
+            dcc.Tab(label='SOUTHEAST', value='Southeast',
+                    style=tab_style, selected_style=tab_selected_style),
 
-            dcc.Tab(label='NORTHWEST', value='Northwest', style=tab_style, selected_style=tab_selected_style),
-            dcc.Tab(label='PACIFIC', value='Pacific', style=tab_style, selected_style=tab_selected_style),
-            dcc.Tab(label='SOUTHWEST', value='Southwest', style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='NORTHWEST', value='Northwest',
+                    style=tab_style, selected_style=tab_selected_style),
+            dcc.Tab(label='PACIFIC', value='Pacific', style=tab_style,
+                    selected_style=tab_selected_style),
+            dcc.Tab(label='SOUTHWEST', value='Southwest',
+                    style=tab_style, selected_style=tab_selected_style),
         ], style=tabs_styles),
 
         # html.Button('Refresh Dashboard', id='my-button'),
@@ -221,10 +314,10 @@ app.layout = update_layout()
 @app.callback(
     Output('table-container', 'children'),
     [Input('div-tabs', 'value')])
-
 def update_graph(value):
-    teams = loadData(teamsQuery)
-    teams.columns = ['PlayerId', 'TeamId', 'TeamCode', 'FirstName', 'LastName', 'FullName', 'TeamLogo', 'PlayerImg', 'Division', 'Conference']
+    teams = loadData(teamRosters)
+    teams.columns = ['PlayerId', 'TeamId', 'TeamCode', 'FirstName', 'LastName',
+                     'FullName', 'TeamLogo', 'PlayerImg', 'Division', 'Conference']
     teams = teams[teams['Division'] == value]
     teamdf = parseTeams(teams)
 
