8364b4e9099d895995ef7e99dee7679952ba3d1d
Pratik <thankip9@outlook.com>
2018-12-23 01:36:41 +0000

    Update CurrentRoster.py

--- CurrentRoster.py
+++ CurrentRoster.py
@@ -1,8 +1,14 @@
-import pandas
+import pandas as pd
 import requests 
 from Settings import *
 
 teams = ['1610612737', '1610612738', '1610612751', '1610612766', '1610612741', '1610612739', '1610612765', '1610612754', '1610612748', '1610612749', '1610612752', '1610612753', '1610612755', '1610612761', '1610612764', '1610612742', '1610612743', '1610612744', '1610612745', '1610612746', '1610612747', '1610612763', '1610612750', '1610612740', '1610612760', '1610612756', '1610612757', '1610612758', '1610612759', '1610612762']
+
+url = 'https://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=2018-19&TeamID=1610612737'
+rosters = requests.request('GET', url, headers=headers)
+rosters = rosters.json()
+pd.DataFrame(rosters['resultSets'][0]['rowSet'], columns=list(rosters['resultSets'][0]['headers']))
+
 
 tmp = []
 for t in teams:
@@ -16,8 +22,6 @@ for t in teams:
 
     except ValueError as e:
         print(e)
-
-        rosters = []
 
 
 roster = pd.DataFrame(tmp, columns=list(rosters['resultSets'][0]['headers']))
