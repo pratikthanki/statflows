73f18526411f8c113716aa73d2ed2c43dbd127cb
Pratik Thanki <thankip9@outlook.com>
2019-07-28 14:52:28 +0100

    Update Players.py

--- Players.py
+++ Players.py
@@ -3,35 +3,64 @@ import json
 import pandas as pd
 import pyodbc
 from sqlalchemy import create_engine
+import time
 from Settings import *
 
 
-ms_sql = ms_sql
-# engine = create_engine('mssql+pyodbc://' + ms_sql)
-cursor = engine.connect()
+def SQLServerConnection(config):
+    conn_str = (
+        'DRIVER={driver};SERVER={server},{port};DATABASE={database};UID={username};PWD={password}')
+
+    conn = pyodbc.connect(
+        conn_str.format(**config)
+    )
+
+    return conn
+
+
+conn = SQLServerConnection(sqlconfig)
+
+
+def loadData(query):
+    sqlData = []
+
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        pass
+    except Exception as e:
+        rows = pd.read_sql(query, conn)
+
+    for row in rows:
+        sqlData.append(list(row))
+
+    df = pd.DataFrame(sqlData)
+
+    return df
 
 
 # Create dataframe from sql table in database
-Players = pd.read_sql("select * from [dbo].[Players]", conn)
+Players = loadData('SELECT * FROM [dbo].[Players] WHERE PlayerID != 0')
+Players.columns = ['PlayerID', 'FirstName', 'LastName']
 Players = Players[:10]
 
 
 # --------------------------- Player Info Rwquest ---------------------------
 
-headers = headers
-
 playerInfo = []
-url1 = Players_url1
-for i in Players['PlayerID']:
+for i in Players['PlayerID'].values:
     try:
-        playerInfoRequest = requests.get(url1 + str(i), headers=headers)
-        print(str(i) + ' - ' + str(playerInfoRequest.status_code))
+        time.sleep(5)
+        playerInfoRequest = requests.get(Players_url1 + str(i), headers=headers)
+        print(str(i), str(playerInfoRequest.status_code))
         #playerInfoRequest.raise_for_status()
         playerInfoRequest = playerInfoRequest.json()
         playerInfo.append(playerInfoRequest)
         pass
-    except ValueError:
-        print(str(i) + 'Error during request')
+    except ValueError as e:
+        print(i, e)
 
 
 
@@ -49,6 +78,4 @@ for i in playerInfo:
 
 playerTable = pd.DataFrame(playerTable)
 playerHeaders = playerInfo[0]['resultSets'][0]['headers']
-
 playerTable.columns = playerHeaders
-
