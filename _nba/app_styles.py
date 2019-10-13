27ed8b323da53d58cb0eeb4dda8702a61f21056c
Pratik Thanki <thankip9@outlook.com>
2019-10-13 23:21:30 +0100

    Create app_styles.py

---
+++ _nba/app_styles.py
@@ -0,0 +1,53 @@
+DEFAULT_IMAGE = 'https://stats.nba.com/media/img/league/nba-headshot-fallback.png'
+
+HEADER_STYLE = {
+    'align': 'center',
+    'width': '300px',
+    'background-color': '#0f6db5',
+    'text-align': 'center',
+    'font-size': '20px',
+    'padding': '20px',
+    'color': '#ffffff'}
+
+TABLE_STYLE = {
+    'display': 'table',
+    'border-cllapse': 'separate',
+    'font': '15px Open Sans, Arial, sans-serif',
+    'font-weight': '30',
+    'width': '100%'}
+
+ALL_TAB_STYLE = {
+    'height': '50px',
+    'padding': '15px'}
+
+SINGLE_TAB_STYLE = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '10px',
+    'font-size': '15px'}
+
+SELECTED_TAB_STYLE = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'fontWeight': 'bold',
+    'color': 'white',
+    'padding': '5px',
+    'font-size': '22px'}
+
+EVENT_DEFINITIONS = {
+    '0': 'Game End',
+    '1': 'Shot Made',
+    '2': 'Shot Missed',
+    '3': 'Free Throw',
+    '4': 'Rebound',
+    '5': 'Turnover',
+    '6': 'Foul',
+    '7': 'Violation',
+    '8': 'Substitution',
+    '9': 'Timeout',
+    '10': 'Jump Ball',
+    '11': 'Ejection',
+    '12': 'Start Period',
+    '13': 'End Period',
+    '18': 'Instant Replay',
+    '20': 'Stoppage: Out-of-Bounds'}
