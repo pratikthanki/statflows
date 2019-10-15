cb20135946a15e78131f5b6c39fd3af520d1298d
Pratik Thanki <thankip9@outlook.com>
2019-10-15 23:18:17 +0100

    Update sql_queries.py

--- _nba/sql_queries.py
+++ _nba/sql_queries.py
@@ -5,3 +5,16 @@ shot_chart_query = 'EXEC [dbo].[sp_PlayerShotChart]'
 standings_query = 'SELECT * FROM [dbo].[vwStandings]'
 team_game_stats_query = 'EXEC [dbo].[sp_TeamGameStats]'
 team_season_stats_query = 'EXEC [dbo].[sp_TeamSeasonStats]'
+
+SHOT_PLOT_COLUMNS = ['ClockTime', 'Description', 'EType', 'Evt', 'LocationX',
+                     'LocationY', 'Period', 'TeamID', 'PlayerID']
+
+TEAM_COLUMNS = ['TeamID', 'TeamCode', 'TeamLogo']
+
+TEAM_STATS_COLUMNS = ['tid', 'teamcode', 'season', 'ast', 'games', 'blk', 'blka', 'dreb', 'fbpts', 'fbptsa',
+                      'fbptsm', 'fga', 'fgm', 'fta', 'ftm', 'oreb', 'pf', 'pip', 'pipa', 'pipm',
+                      'pts', 'reb', 'stl', 'tov', 'tpa', 'tpm']
+
+CURRENT_ROSTER_COLUMNS = ['TeamId', 'Season', 'LeagueId', 'Player', 'JerseyNumber', 'Position', 'Height', 'Weight',
+                          'DoB', 'Age', 'Experience', 'School', 'PlayerId', 'TeamLogo', 'PlayerImg', 'Division',
+                          'Conference']
