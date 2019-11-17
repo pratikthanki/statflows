2277488cdbb12f94bc8e6a4080fecbb1c98569de
Pratik Thanki <thankip9@outlook.com>
2019-11-17 20:33:31 +0000

    Update standings.py

--- _nba/standings.py
+++ _nba/standings.py
@@ -1,43 +1,57 @@
-import requests
-import json
-import pandas as pd
 import pyodbc
+import logging
+import requests
 from shared_config import sql_config
 from nba_settings import standings_1, headers
-from shared_modules import sql_server_connection, execute_sql
+from shared_modules import sql_server_connection, execute_sql, create_logger
 
-standingsRequest = requests.request('GET', standings_1, headers=headers)
-standingsRequest = standingsRequest.json()
 
-standings = []
-for i in standingsRequest['payload']['standingGroups']:
-    for j in i['teams']:
-        standings.append({
-            'Abbr': j['profile']['abbr'],
-            'Conference': j['profile']['conference'],
-            'TeamId': j['profile']['id'],
-            'TeamName': j['profile']['name'],
-            'ConfRank': j['standings']['confRank'],
-            'Wins': j['standings']['wins'],
-            'Losses': j['standings']['losses'],
-            'Streak': j['standings']['streak'],
-            'Win%': j['standings']['winPct'],
-            'DivLosses': j['standings']['divLoss'],
-            'DivWins': j['standings']['divWin'],
-            'DivRank': j['standings']['divRank'],
-            'Last10': j['standings']['last10'],
-            'HomeWins': j['standings']['homeWin'],
-            'HomeLosses': j['standings']['homeLoss'],
-            'RoadWins': j['standings']['roadWin'],
-            'RoadLosses': j['standings']['roadLoss'],
-            'RoadStreak': j['standings']['roadStreak'],
-            'HomeStreak': j['standings']['homeStreak']
-        })
+def get_standings(cursor):
+    standings_request = requests.request('GET', standings_1, headers=headers)
+    standings_request = standings_request.json()
 
-conn, cursor = sql_server_connection(sql_config, 'nba')
-cursor.execute('DELETE FROM LeagueStandings')
+    standings = []
+    for i in standings_request['payload']['standingGroups']:
+        for j in i['teams']:
+            print(j['profile']['id'])
+            standings.append({
+                'Abbr': j['profile']['abbr'],
+                'Conference': j['profile']['conference'],
+                'TeamId': j['profile']['id'],
+                'TeamName': j['profile']['name'],
+                'ConfRank': j['standings']['confRank'],
+                'Wins': j['standings']['wins'],
+                'Losses': j['standings']['losses'],
+                'Streak': j['standings']['streak'],
+                'Win%': j['standings']['winPct'],
+                'DivLosses': j['standings']['divLoss'],
+                'DivWins': j['standings']['divWin'],
+                'DivRank': j['standings']['divRank'],
+                'Last10': j['standings']['last10'],
+                'HomeWins': j['standings']['homeWin'],
+                'HomeLosses': j['standings']['homeLoss'],
+                'RoadWins': j['standings']['roadWin'],
+                'RoadLosses': j['standings']['roadLoss'],
+                'RoadStreak': j['standings']['roadStreak'],
+                'HomeStreak': j['standings']['homeStreak']
+            })
 
-execute_sql('LeagueStandings', standings, ['TeamName'], cursor)
-conn.commit()
+    execute_sql('LeagueStandings', standings, ['TeamName'], cursor)
 
-print('Task completed')
+
+def main():
+    create_logger(__file__)
+    logging.info('Task started')
+
+    conn, cursor = sql_server_connection(sql_config, 'nba')
+
+    cursor.execute('DELETE FROM LeagueStandings')
+    get_standings(cursor)
+
+    conn.commit()
+
+    logging.info('Task completed')
+
+
+if __name__ == '__main__':
+    main()
