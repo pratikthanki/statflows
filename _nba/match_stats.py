b1bc65e81b824cba9503afbd43848b9cc08e0c63
Pratik Thanki <thankip9@outlook.com>
2019-12-01 19:33:21 +0000

    Update match_stats.py

--- _nba/match_stats.py
+++ _nba/match_stats.py
@@ -1,36 +1,45 @@
 import logging
 import time
 from datetime import datetime, timedelta
-from shared_modules import create_logger, get_data, remove_duplicates, MongoConnection
+from shared_modules import create_logger, get_data, MongoConnection
 from nba_settings import current_season_1, current_season_2, current_season_3
 
+upsert_keys = {
+    'games': ['game_id'],
+    'match_stats': ['pid', 'gid', 'tid'],
+    'pbp': ['evt', 'gid', 'gid', 'pid', 'tid']
+}
 
-def get_schedule(url, offset):
+
+def get_schedule(url, mongodb_connector, nba_db, offset):
     game_rqst = get_data(url)
+
     now = datetime.strptime(time.strftime('%Y-%m-%d'), '%Y-%m-%d')
     date_offset = now - timedelta(days=offset)
 
-    game = []
+    games = []
     for i in game_rqst['lscd']:
         for j in i['mscd']['g']:
-            if date_offset <= datetime.strptime(j['gdte'], '%Y-%m-%d') < now:
-                game.append({
-                    'GameID': j['gid'],
-                    'GameCode': j['gcode'],
-                    'Venue': j['an'],
-                    'Date': j['gdte'],
-                    'DateString': j['gcode'].split('/')[0],
-                    'AwayTeamID': j['v']['tid'],
-                    'AwayScore': j['v']['s'],
-                    'HomeTeamID': j['h']['tid'],
-                    'HomeScore': j['h']['s']
+            game_date = datetime.strptime(j['gdte'], '%Y-%m-%d')
+            if date_offset < game_date < now:
+                games.append({
+                    'game_id': j['gid'],
+                    'game_code': j['gcode'],
+                    'venue': j['an'],
+                    'date': j['gdte'],
+                    'date_string': j['gcode'].split('/')[0],
+                    'away_team_id': j['v']['tid'],
+                    'away_score': j['v']['s'],
+                    'home_team_id': j['h']['tid'],
+                    'home_score': j['h']['s']
                 })
-    return game
+
+    mongodb_connector.insert_documents(nba_db, nba_db['games'], games, upsert_keys['games'])
+    return [i['game_id'] for i in games]
 
 
-def get_game_stats(url, url_prop, games):
+def get_game_stats(url, url_prop, list_of_games):
     game_stats = []
-    list_of_games = [i['GameID'] for i in games]
     for i in list_of_games:
         try:
             stats_url = '{0}{1}_{2}.json'.format(url, i, url_prop)
@@ -60,11 +69,10 @@ def game_detail_stats(game_json, mongodb_connector, nba_db):
                     c['ta'] = b[prop]['ta']
                     lst.append(c)
 
-    mongodb_connector.insert_documents(nba_db, nba_db['match_stats'], lst)
-    roster_details(lst, mongodb_connector, nba_db)
+    mongodb_connector.insert_documents(nba_db, nba_db['match_stats'], lst, upsert_keys['match_stats'])
 
 
-def game_pbp_stats(game_json):
+def game_pbp_stats(game_json, mongodb_connector, nba_db):
     play_by_play = []
     for i in game_json:
         if i is None:
@@ -80,41 +88,28 @@ def game_pbp_stats(game_json):
                 k['mid'] = i['g']['mid']
                 play_by_play.append(k)
 
-    return play_by_play
+    mongodb_connector.insert_documents(nba_db, nba_db['match_pbp'], play_by_play, upsert_keys['pbp'])
 
 
-def roster_details(game_json, mongodb_connector, nba_db):
-    players = [{'PlayerID': i['pid'], 'LastName': i['ln'], 'FirstName': i['fn']} for i in game_json]
-    teams = [{'TeamID': i['tid'], 'TeamCode': i['ta']} for i in game_json]
-
-    players = remove_duplicates(players)
-    teams = remove_duplicates(teams)
-
-    mongodb_connector.insert_documents(nba_db, nba_db['players'], players)
-    mongodb_connector.insert_documents(nba_db, nba_db['teams'], teams)
-
-
-def main():
-    create_logger(__file__)
-    logging.info('Task started')
-
-    mongodb_connector = MongoConnection()
+def update_stats(mongodb_connector, season):
     nba_db = mongodb_connector.db_connect('nba')
 
-    season = '2019'
-
-    # for season in ['2016', '2017', '2018']:
-    #     print(f'Getting data for {season} season')
-
-    games = get_schedule(current_season_1.format(season), offset=14)
-    mongodb_connector.insert_documents(nba_db, nba_db['games'], games)
+    games = get_schedule(current_season_1.format(season), mongodb_connector, nba_db, offset=14)
 
     game_detail_json = get_game_stats(current_season_2.format(season), 'gamedetail', games)
     game_detail_stats(game_detail_json, mongodb_connector, nba_db)
 
     game_pbp_json = get_game_stats(current_season_3.format(season), 'full_pbp', games)
-    play_by_play = game_pbp_stats(game_pbp_json)
-    mongodb_connector.insert_documents(nba_db, nba_db['match_pbp'], play_by_play)
+    game_pbp_stats(game_pbp_json, mongodb_connector, nba_db)
+
+
+def main():
+    create_logger(__file__)
+
+    logging.info('Task started')
+
+    mongodb_connector = MongoConnection()
+    update_stats(mongodb_connector, season='2019')
 
     logging.info('Task completed')
 
