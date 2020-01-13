443f3bcbd3e57749af20991dc5ee0b1bfa31e32a
Pratik Thanki <thankip9@outlook.com>
2020-01-13 21:47:30 +0000

    Update stats.py

--- _nfl/stats.py
+++ _nfl/stats.py
@@ -1,7 +1,8 @@
 import requests
 import collections
 import pandas as pd
-from nfl_settings import base_url, stat_types, season_phases, headers
+from shared_modules import create_logger, get_data, MongoConnection
+from nfl_settings import base_url, stat_types, headers, mongo_details, upsert_keys
 
 pd.set_option('max_columns', 50)
 pd.set_option('max_rows', 600)
@@ -19,14 +20,16 @@ def flatten(d, parent_key='', sep='_'):
     return dict(items)
 
 
-def parse_stats(data):
+def parse_stats(data, mongodb_connector, nfl_db):
     output = []
     for week in data:
         for stats in week['stats']:
             output.append(flatten(stats))
 
+    # mongodb_connector.insert_documents(nfl_db, nfl_db['match_pbp'], output, upsert_keys)
 
-def get_stats(season, stat):
+
+def get_stats(season, stat, mongodb_connector, nfl_db):
     play_stats = []
     for week in range(1, 23):
         season_phase = 'REG' if week <= 17 else 'POST'
@@ -37,12 +40,19 @@ def get_stats(season, stat):
         print(season, week, query.status_code)
         play_stats.append(query.json())
 
-    parse_stats(play_stats)
+    parse_stats(play_stats, mongodb_connector, nfl_db)
 
 
 def main():
+    create_logger(__file__)
+
+    project = 'match-stats'
+    mongodb_connector = MongoConnection(project=project)
+    nfl_db = mongodb_connector.db_connect('nfl')
+
+    season = 2019
     for stat in stat_types:
-        get_stats(2019, stat)
+        get_stats(season, stat, mongodb_connector, nfl_db)
 
 
 if __name__ == '__main__':
